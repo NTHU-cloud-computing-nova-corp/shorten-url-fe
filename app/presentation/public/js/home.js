fe5560c3d2daf0e4f6c46685a013325c5690b342
SarunWu <96522041+SarunWu@users.noreply.github.com>
2022-06-09 19:25:39 +0800

    Update component ids

--- app/presentation/public/js/home.js
+++ app/presentation/public/js/home.js
@@ -1,4 +1,4 @@
 function copy_short_url() {
-    const short_url = document.getElementById('short_url').value;
+    const short_url = document.getElementById('short_url_output').value;
     navigator.clipboard.writeText(short_url);
 }
